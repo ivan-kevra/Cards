4b8aaa85e50315389e7cbae8da64a8859149551e
ivan kevra <vanyakevra@gmail.com>
2023-05-01 00:18:19 +0300

    fix route

--- src/components/sign-up.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const SignIn = () => {
-  return <div>Sign in</div>;
-};
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "app/store";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const container = document.getElementById('root')!;
+const router = createBrowserRouter([
+  { path: "/", element: <div>Hello world!</div> },
+  { path: "/login", element: <h1>Login</h1> },
+  { path: "/register", element: <h1>Register</h1> },
+  { path: "/packs", element: <h1>Packs</h1> },
+]);
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
