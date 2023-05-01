3bdecd7fede9cbd30bbf27737d4d5c93536a3d46
ivan kevra <vanyakevra@gmail.com>
2023-05-01 12:52:35 +0300

    update login

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -3,6 +3,16 @@ import { useAppDispatch } from "app/hooks";
 import { authThunks } from "features/auth/auth.slice";
 import { Header } from "features/auth/login/header/Header";
 import style from "./Style.module.css";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Input from "@mui/material/Input";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
@@ -14,19 +24,50 @@ export const Login = () => {
     };
     dispatch(authThunks.login(payload));
   };
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
   return (
-    <div>
+    <div className={style.login}>
       <Header loginHandler={loginHandler} />
+
       <div className={style.container}>
         <h1>Sign in</h1>
-        <span>Email</span>
-        <span>Password</span>
-        <span>Remember me</span>
-        <span>Forgot password</span>
-        <span>Forgot password</span>
-        <span>sign in</span>
-        <span>Don`t have an account?</span>
-        <span>Sign up</span>
+        <TextField
+          value={"vanyakevra@gmail.com"}
+          id="standard-basic"
+          label="Email"
+          variant="standard"
+          sx={{ m: 1, width: "85%" }}
+        />
+        <FormControl sx={{ m: 1, width: "85%" }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            value={"12345678"}
+            id="standard-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <div className={style.rememberMe}>
+          <Checkbox defaultChecked />
+          <span>Remember me</span>
+        </div>
+        <span className={style.forgotPassword}>Forgot password?</span>
+
+        <button onClick={loginHandler} className={style.button}>
+          <span>Sign in</span>
+        </button>
+
+        <span className={style.noAccount}>Don`t have an account?</span>
+        <Button variant="text">Sign up</Button>
       </div>
     </div>
   );
--- src/features/auth/login/header/Header.tsx
+++ src/features/auth/login/header/Header.tsx
@@ -1,5 +1,4 @@
 import logo from "features/auth/login/img/logo.svg";
-import Button from "@mui/material/Button";
 import React from "react";
 import style from "./Style.module.css";
 
@@ -10,9 +9,9 @@ export const Header = (props: HeaderPropsType) => {
   return (
     <div className={style.header}>
       <img src={logo} alt={"logo"} />
-      <Button variant="contained" onClick={props.loginHandler} className={style.button}>
-        Sign in
-      </Button>
+      <button onClick={props.loginHandler} className={style.button}>
+        <span>Sign in</span>
+      </button>
     </div>
   );
 };
