9da91f3fa523291b33ae83c9a45a0a3ddad7c841
ivan kevra <vanyakevra@gmail.com>
2023-05-02 14:26:29 +0300

    add login logic

--- src/features/auth/login/LoginCopy.tsx
+++ src/features/auth/login/LoginCopy.tsx
@@ -1,46 +1,41 @@
-import { SubmitHandler, useForm } from "react-hook-form";
+import React from "react";
 import { useAppDispatch } from "app/hooks";
 import { authThunks } from "features/auth/auth.slice";
-import { ArgLoginType } from "features/auth/auth.api";
-import React from "react";
-import style from "features/auth/login/Style.module.css";
-import logo from "features/auth/login/img/logo.svg";
 import { Header } from "features/auth/login/header/Header";
+import style from "./Style.module.css";
 import TextField from "@mui/material/TextField";
-import InputLabel from "@mui/material/InputLabel";
-import Input from "@mui/material/Input";
-import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
 import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Input from "@mui/material/Input";
 import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ArgLoginType>();
-  const onSubmit: SubmitHandler<ArgLoginType> = (data) => {
-    console.log(data);
-    dispatch(authThunks.login(data));
+  const loginHandler = () => {
+    const payload = {
+      email: "vanyakevra@gmail.com",
+      password: "12345678",
+      rememberMe: false,
+    };
+    dispatch(authThunks.login(payload));
   };
   const [showPassword, setShowPassword] = React.useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={style.login}>
-      <Header />
+    <div className={style.login}>
+      {/*<Header onSubmit={loginHandler} />*/}
 
       <div className={style.container}>
         <h1>Sign in</h1>
         <TextField
-          defaultValue={"vanyakevra@gmail.com"}
-          {...register("email", { required: true })}
+          value={"vanyakevra@gmail.com"}
           id="standard-basic"
           label="Email"
           variant="standard"
@@ -49,8 +44,7 @@ export const Login = () => {
         <FormControl sx={{ m: 1, width: "85%" }} variant="standard">
           <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
           <Input
-            defaultValue={"12345678"}
-            {...register("password", { required: true, minLength: 8 })}
+            value={"12345678"}
             id="standard-adornment-password"
             type={showPassword ? "text" : "password"}
             endAdornment={
@@ -68,12 +62,13 @@ export const Login = () => {
         </div>
         <span className={style.forgotPassword}>Forgot password?</span>
 
-        <button type="submit" className={style.button}>
+        <button onClick={loginHandler} className={style.button}>
           <span>Sign in</span>
         </button>
+
         <span className={style.noAccount}>Don`t have an account?</span>
         <Button variant="text">Sign up</Button>
       </div>
-    </form>
+    </div>
   );
 };
--- src/features/auth/login/header/Header.tsx
+++ src/features/auth/login/header/Header.tsx
@@ -2,14 +2,11 @@ import logo from "features/auth/login/img/logo.svg";
 import React from "react";
 import style from "./Style.module.css";
 
-type HeaderPropsType = {
-  loginHandler: () => void;
-};
-export const Header = (props: HeaderPropsType) => {
+export const Header = () => {
   return (
     <div className={style.header}>
       <img src={logo} alt={"logo"} />
-      <button onClick={props.loginHandler} className={style.button}>
+      <button type="submit" className={style.button}>
         <span>Sign in</span>
       </button>
     </div>
