e741fd36525e065d5b168d7fae1ea48f06ae090d
ivan kevra <vanyakevra@gmail.com>
2023-06-06 15:33:33 +0300

    fix

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,17 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
-import { appReducer } from "app/app.slice";
-import { authReducer } from "features/auth/auth.slice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    app: appReducer,
-    auth: authReducer,
+    counter: counterReducer
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "app/store";
+import { store } from "./app/store";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { App } from "app/App";
-import { Register } from "features/auth/register/Register";
-import { Login } from "features/auth/login/Login";
-import { RestorePassword } from "features/auth/restorePassword/RestorePassword";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
-  { path: "/restorePassword", element: <RestorePassword /> },
-  { path: "/packs", element: <h1>Packs</h1> },
-]);
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
