4b321caf7bd3026d44c355be9f187d96d623c0df
Ivan Kevra <i.kevra@horizont-rnd.by>
2024-01-08 10:05:26 +0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["@it-incubator/eslint-config", "plugin:storybook/recommended"],
+  overrides: [
+    {
+      files: ["**/*.stories.tsx"],
+      rules: {
+        "no-console": "off",
+        "react-hooks/rules-of-hooks": "off",
+      },
+    },
+  ],
+};
